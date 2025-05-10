a7f0cf1f599558522d245baf17c78b7681cb9dc3
J-Rios <jrios.github@gmail.com>
2025-05-10 21:16:03 +0200

    Update bash ssh auth check script for Ubuntu 24.04

--- ssh_check_logins.sh
+++ ssh_check_logins.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-cat /var/log/auth.log | grep "sshd" | grep -e "Accepted password for " -e "Accepted publickey for " | awk '{print $1 " " $2 " " $3 " - " $11 ":" $13}'
-
+cat /var/log/auth.log | grep "sshd" | grep -e "Accepted password for " -e "Accepted publickey for " | awk '{print $1 " " $2 " " $7 " - " $9 ":" $11}'
 exit 0
